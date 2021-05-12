949773c528bfb405af11f504cb47619321fab1fe
symlara <40181569+symlara@users.noreply.github.com>
2021-05-12 16:32:53 -0500

    added stripe functionality

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,87 +1,108 @@
 import React, { useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLazyQuery } from '@apollo/react-hooks';
+import { QUERY_CHECKOUT } from "../../utils/queries"
+import { idbPromise } from "../../utils/helpers"
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
+import { useStoreContext } from "../../utils/GlobalState";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
 
-import { idbPromise } from '../../utils/helpers';
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
-// use the custom useStoreContext Hook to establish a state variable and the dispatch() function to update the state. 
-import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+const Cart = () => {
+  const [state, dispatch] = useStoreContext();
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session })
+      })
+    }
+  }, [data]);
 
-
-    const Cart = () => {
-        const [state, dispatch] = useStoreContext();
-        //checking to see if state.cart.length is 0, then executing getCart() to retrieve the items from the cart object store and save it to the global state object
-        useEffect(() => {
-            async function getCart() {
-                const cart = await idbPromise('cart', 'get');
-                dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
-            };
-
-            if (!state.cart.length) {
-                getCart();
-            }
-        }, [state.cart.length, dispatch]);
-      
-        function toggleCart() {
-          dispatch({ type: TOGGLE_CART });
-        }
-
-    //This function will add up the prices of everything saved in state.cart
-    function calculateTotal() {
-        let sum = 0;
-        state.cart.forEach(item => {
-          sum += item.price * item.purchaseQuantity;
-        });
-        return sum.toFixed(2);
-      }
-    
-      if (!state.cartOpen) {
-        return (
-          <div className="cart-closed" onClick={toggleCart}>
-            <span
-              role="img"
-              aria-label="trash">🛒</span>
-          </div>
-        );
-      }
-    
-      return (
-        <div className="cart">
-          <div className="close" onClick={toggleCart}>[close]</div>
-          <h2>Shopping Cart</h2>
-          {state.cart.length ? (
-            <div>
-              {state.cart.map(item => (
-                <CartItem key={item._id} item={item} />
-              ))}
-    
-              <div className="flex-row space-between">
-                <strong>Total: ${calculateTotal()}</strong>
-    
-                {
-                  Auth.loggedIn() ?
-                    <button>
-                      Checkout
-                  </button>
-                    :
-                    <span>(log in to check out)</span>
-                }
-              </div>
-            </div>
-          ) : (
-              <h3>
-                <span role="img" aria-label="shocked">
-                  😱
-              </span>
-              You haven't added anything to your cart yet!
-              </h3>
-            )}
-        </div>
-      );
+  useEffect(() => {
+    async function getCart() {
+      const cart = await idbPromise('cart', 'get');
+      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
     };
-    
-    export default Cart;
-    
+
+    if (!state.cart.length) {
+      getCart();
+    }
+  }, [state.cart.length, dispatch]);
+
+  function toggleCart() {
+    dispatch({ type: TOGGLE_CART });
+  }
+
+  function calculateTotal() {
+    let sum = 0;
+    state.cart.forEach(item => {
+      sum += item.price * item.purchaseQuantity;
+    });
+    return sum.toFixed(2);
+  }
+
+  function submitCheckout() {
+    const productIds = [];
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds }
+    });
+  }
+
+  if (!state.cartOpen) {
+    return (
+      <div className="cart-closed" onClick={toggleCart}>
+        <span
+          role="img"
+          aria-label="trash">🛒</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="cart">
+      <div className="close" onClick={toggleCart}>[close]</div>
+      <h2>Shopping Cart</h2>
+      {state.cart.length ? (
+        <div>
+          {state.cart.map(item => (
+            <CartItem key={item._id} item={item} />
+          ))}
+
+          <div className="flex-row space-between">
+            <strong>Total: ${calculateTotal()}</strong>
+
+            {
+              Auth.loggedIn() ?
+                <button onClick={submitCheckout}>
+                  Checkout
+              </button>
+                :
+                <span>(log in to check out)</span>
+            }
+          </div>
+        </div>
+      ) : (
+          <h3>
+            <span role="img" aria-label="shocked">
+              😱
+          </span>
+          You haven't added anything to your cart yet!
+          </h3>
+        )}
+    </div>
+  );
+};
+
+export default Cart;
--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,30 +1,27 @@
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_CATEGORIES } from "../../utils/queries";
-import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
-
 import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
 
   const { categories } = state;
-  
+
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
-  
+
   useEffect(() => {
-    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
     if (categoryData) {
-      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
       dispatch({
-        ttype: UPDATE_CATEGORIES,
+        type: UPDATE_CATEGORIES,
         categories: categoryData.categories
       });
       categoryData.categories.forEach(category => {
         idbPromise('categories', 'put', category);
       });
-    }else if (!loading) {
+    } else if (!loading) {
       idbPromise('categories', 'get').then(categories => {
         dispatch({
           type: UPDATE_CATEGORIES,
@@ -32,15 +29,14 @@ function CategoryMenu() {
         });
       });
     }
-    
-  }, [categoryData, dispatch]);
+  }, [categoryData, loading, dispatch]);
 
- const handleClick = id => {
-   dispatch({
-     type: UPDATE_CURRENT_CATEGORY,
-     currentCategory: id
-   });
- };
+  const handleClick = id => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
+    });
+  };
 
   return (
     <div>
